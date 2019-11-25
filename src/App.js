4409308cdc229c6747023ff396df5b048ab157ab
sandipamte <58175947+sandipamte@users.noreply.github.com>
2019-11-25 23:12:24 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Feature Branch Testing
+          This is my first Docker based application :-)
         </p>
         <a
           className="App-link"
@@ -23,3 +23,4 @@ function App() {
   );
 }
 
+export default App;
